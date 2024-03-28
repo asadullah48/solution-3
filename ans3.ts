4012fe0ce5a161174b2de881ec29d1d3375e07bb
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-03-28 12:10:23 +0500

    Initial commit

---
+++ ans3.ts
@@ -0,0 +1,5 @@
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let text: string = "ahmed";
+console .log(text.toLowerCase());
+console .log(text.toUpperCase());
+console .log(text.charAt(0) .toUpperCase() + text .slice(1));
